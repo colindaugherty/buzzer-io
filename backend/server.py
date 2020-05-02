9670b8b7d2271136ff8364c81c45f05c50d30773
Colin Daugherty <colinddaugherty@gmail.com>
2020-05-02 00:42:34 -0400

    trying to get flask backend working

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config["SERVER_NAME"] = ["buzz.colindaugherty.net"]
+
 @app.route("/api")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
@@ -10,4 +12,4 @@ def test():
     return "<h1 style='color:blue'>General Kenobi!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
