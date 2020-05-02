30c16b9fdc2c637195d5f8a5b3d0baefc3a41295
Colin Daugherty <colinddaugherty@gmail.com>
2020-05-02 00:37:40 -0400

    testing nginx conf

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+@app.route("/api/tests")
+def test():
+    return "<h1 style='color:blue'>General Kenobi!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
